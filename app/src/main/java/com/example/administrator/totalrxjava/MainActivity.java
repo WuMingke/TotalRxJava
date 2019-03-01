7fc8f09248f32edac2804c1ac819abcf27603d5f
wumingke <766190553@qq.com>
2019-03-01 16:14:18 +0800

    create

--- app/src/main/java/com/example/administrator/totalrxjava/MainActivity.java
+++ app/src/main/java/com/example/administrator/totalrxjava/MainActivity.java
@@ -2,6 +2,13 @@ package com.example.administrator.totalrxjava;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.functions.Consumer;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +16,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> emitter) throws Exception {
+                emitter.onNext("Hello World");
+            }
+        }).subscribe(new Consumer<String>() {
+            @Override
+            public void accept(String s) throws Exception {
+                ((TextView) findViewById(R.id.world)).setText(s);
+            }
+        });
+
     }
 }
